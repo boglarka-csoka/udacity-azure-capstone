1e1535fd6538213a1933cd303f0915d786c3100e
boglarka-csoka <104906755+boglarka-csoka@users.noreply.github.com>
2022-05-24 15:33:56 +0000

    Add files via upload

--- entry_script.py
+++ entry_script.py
@@ -7,13 +7,15 @@ from azureml.core.model import Model
 
 def init():
     global model
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'),'logreg_model.pkl')
+    #model=ws.models['hyperparam_best_model']
+    #model_path=model.download(exist_ok=True)
+    model_path =os.path.join(os.getenv("AZUREML_MODEL_DIR"), "logreg_model.pkl") #Model.get_model_path(model_name='logreg_model.pkl')
     model = joblib.load(model_path)
 
 def run(data_raw):
     try:
         #data = json.load(open('data.json'))
-        data = np.array(data_raw['data'])
+        data = np.array(json.loads(data_raw)['data']) #.tolist()
         #data=data_raw['data']
         # Call predict() on each model
         result = model.predict(data)
